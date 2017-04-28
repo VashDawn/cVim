750f95838445e99453952489567186f3d0a60a94
vash <st.vash@outlook.com>
2017-04-28 22:48:05 +0800

    incremental test_get_vimDir.sh

--- test_get_vimDir.sh
+++ test_get_vimDir.sh
@@ -2,3 +2,10 @@
 VIM=`vim --version | grep -oP '(?<=\$VIM: ").+(?=")'`
 
 echo $VIM
+
+# The codes above not worked.
+# The follow will be work.
+
+vim --version | grep -oP '(?<=\$VIM: ").+(?=")' > t1.txt
+vimDir=`cat t1.txt`
+
